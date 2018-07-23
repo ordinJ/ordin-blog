984e52456e0f2bdc5762ae3d780d50730bda2ba7
yangxs <yanghbwork@163.com>
2018-07-23 15:13:11 +0800

    init

---
+++ src/main/java/com/yaa/SiteBlogApplication.java
@@ -0,0 +1,12 @@
+package com.yaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteBlogApplication.class, args);
+	}
+}
