d74f94144fc7cbd23687710fe8f127337bcd8b2e
yangxs <yanghbwork@163.com>
2018-07-23 20:22:13 +0800

    集成mybatis

--- src/main/java/com/yaa/SiteBlogApplication.java
+++ src/main/java/com/yaa/SiteBlogApplication.java
@@ -1,9 +1,11 @@
 package com.yaa;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yaa.mapper")
 public class SiteBlogApplication {
 
 	public static void main(String[] args) {
