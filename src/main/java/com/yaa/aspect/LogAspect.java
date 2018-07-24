674e91395c2caed4247f724d1b6e93b2958c3fd2
mac <yanghbwork@163.com>
2018-07-24 21:30:31 +0800

    删除多余引入

--- src/main/java/com/yaa/aspect/LogAspect.java
+++ src/main/java/com/yaa/aspect/LogAspect.java
@@ -1,6 +1,5 @@
 package com.yaa.aspect;
 
-import com.alibaba.fastjson.JSON;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
