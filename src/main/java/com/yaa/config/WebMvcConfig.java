5d5e1a399e143321ce4f64d477c3501f612f5491
yangxs <yanghbwork@163.com>
2019-06-18 17:02:39 +0800

    修复样式

--- src/main/java/com/yaa/config/WebMvcConfig.java
+++ src/main/java/com/yaa/config/WebMvcConfig.java
@@ -5,12 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Arrays;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
     private PathConfig pathConfig;
--- src/test/java/com/yaa/SiteBlogApplicationTests.java
+++ src/test/java/com/yaa/SiteBlogApplicationTests.java
@@ -17,33 +17,33 @@ public class SiteBlogApplicationTests {
 	}
 
 	public static void backup() {
-		try {
-			Runtime rt = Runtime.getRuntime();
-			Process child = rt.exec("D:/mysql/mysql-5.7.16-winx64/bin/mysqldump -uroot -p123456 blog");
-			InputStream in = child.getInputStream();
-			InputStreamReader xx = new InputStreamReader(in, "utf8");
-			String inStr;
-			StringBuffer sb = new StringBuffer("");
-			String outStr;
-			BufferedReader br = new BufferedReader(xx);
-			while ((inStr = br.readLine()) != null) {
-				sb.append(inStr + "\r\n");
-			}
-			outStr = sb.toString();
-
-			FileOutputStream fout = new FileOutputStream("E:/qq.sql");
-			OutputStreamWriter writer = new OutputStreamWriter(fout, "utf8");
-			writer.write(outStr);
-			writer.flush();
-
-			in.close();
-			xx.close();
-			br.close();
-			writer.close();
-			fout.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		try {
+//			Runtime rt = Runtime.getRuntime();
+//			Process child = rt.exec("D:/mysql/mysql-5.7.16-winx64/bin/mysqldump -uroot -p123456 blog");
+//			InputStream in = child.getInputStream();
+//			InputStreamReader xx = new InputStreamReader(in, "utf8");
+//			String inStr;
+//			StringBuffer sb = new StringBuffer("");
+//			String outStr;
+//			BufferedReader br = new BufferedReader(xx);
+//			while ((inStr = br.readLine()) != null) {
+//				sb.append(inStr + "\r\n");
+//			}
+//			outStr = sb.toString();
+//
+//			FileOutputStream fout = new FileOutputStream("E:/qq.sql");
+//			OutputStreamWriter writer = new OutputStreamWriter(fout, "utf8");
+//			writer.write(outStr);
+//			writer.flush();
+//
+//			in.close();
+//			xx.close();
+//			br.close();
+//			writer.close();
+//			fout.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 
